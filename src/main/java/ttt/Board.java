22d985744018a0cdf5b8195b6181ff7b2db3f4cb
Makis Otman <makisotman@gmail.com>
2014-10-13 17:23:13 +0100

    Board returns number of available moves and marks positions

--- src/main/java/ttt/Board.java
+++ src/main/java/ttt/Board.java
@@ -1,13 +1,24 @@
 package ttt;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Board {
-  private int moves;
+  private List<String> grid;
 
   public Board() {
-    moves = 9;
+    grid = new ArrayList<String>(Collections.<String>nCopies(9, null));
   }
 
-  public int getMoves() {
+  public int numberOfAvailableMoves() {
+    return listOfMoves().size();
+  }
+
+  public void markPosition(int position, String mark) {
+    grid.set(position, mark);
+  }
     return moves;
   }
 }
--- src/test/java/ttt/BoardTest.java
+++ src/test/java/ttt/BoardTest.java
@@ -3,15 +3,30 @@ package ttt;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static junit.framework.TestCase.assertEquals;
 
 public class BoardTest {
 
   int defaultAvailableMoves = 9;
+  private Board board;
+
+  @Before
+  public void setup() {
+    board = new Board();
+  }
 
   @Test
   public void itHasNineMoves() {
-    Board board = new Board();
-    assertEquals(defaultAvailableMoves, board.getMoves());
+    assertEquals(defaultAvailableMoves, board.numberOfAvailableMoves());
+  }
+
+  @Test
+  public void itMarksAPositionOnTheGrid() {
+    board.markPosition(0, "X");
+    assertEquals(defaultAvailableMoves - 1, board.numberOfAvailableMoves());
+  }
   }
 }
