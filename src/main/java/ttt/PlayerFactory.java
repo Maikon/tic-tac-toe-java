8e70dec095817dc238e4926261652caf56a7a81e
Makis Otman <makisotman@gmail.com>
2014-11-17 11:11:14 +0000

    Add method in PlayerFactory to return description for all game choices

--- src/main/java/ttt/PlayerFactory.java
+++ src/main/java/ttt/PlayerFactory.java
@@ -6,6 +6,14 @@ import java.util.List;
 import java.util.Map;
 
 public class PlayerFactory {
+  public static final String HUMAN_VS_HUMAN = "1. Human Vs Human";
+  public static final String HUMAN_VS_COMPUTER = "2. Human Vs Computer";
+  public static final String COMPUTER_VS_HUMAN = "3. Computer Vs Human";
+  public static final String COMPUTER_VS_COMPUTER = "4. Computer Vs Computer";
+  public static final String HVH = "1";
+  public static final String HVC = "2";
+  public static final String CVH = "3";
+  public static final String CVC = "4";
   private final Display display;
   private final Map<String, List<Player>> combos;
 
@@ -14,15 +22,24 @@ public class PlayerFactory {
     combos = new HashMap<>();
   }
 
+  public static Map allGameChoices() {
+    Map<String, String> choices = new HashMap<>();
+    choices.put(HVH, HUMAN_VS_HUMAN);
+    choices.put(HVC, HUMAN_VS_COMPUTER);
+    choices.put(CVH, COMPUTER_VS_HUMAN);
+    choices.put(CVC, COMPUTER_VS_COMPUTER);
+    return choices;
+  }
+
   public List<Player> getPlayersForChoice(String choice) {
    return getPlayerCombo().get(choice);
   }
 
   private Map<String, List<Player>> getPlayerCombo() {
-    combos.put("1", createPlayers(new HumanPlayer(display), new HumanPlayer(display)));
-    combos.put("2", createPlayers(new HumanPlayer(display), new ComputerPlayer()));
-    combos.put("3", createPlayers(new ComputerPlayer(), new HumanPlayer(display)));
-    combos.put("4", createPlayers(new ComputerPlayer(), new ComputerPlayer()));
+    combos.put(HVH, createPlayers(new HumanPlayer(display), new HumanPlayer(display)));
+    combos.put(HVC, createPlayers(new HumanPlayer(display), new ComputerPlayer()));
+    combos.put(CVH, createPlayers(new ComputerPlayer(), new HumanPlayer(display)));
+    combos.put(CVC, createPlayers(new ComputerPlayer(), new ComputerPlayer()));
     return combos;
   }
 
--- src/test/java/ttt/PlayerFactoryTest.java
+++ src/test/java/ttt/PlayerFactoryTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PlayerFactoryTest {
@@ -20,29 +21,37 @@ public class PlayerFactoryTest {
 
   @Test
   public void humanVsHumanChoice() {
-    List<Player> players = factory.getPlayersForChoice("1");
+    List<Player> players = factory.getPlayersForChoice(PlayerFactory.HVH);
     assertThat(players.get(0), instanceOf(HumanPlayer.class));
     assertThat(players.get(1), instanceOf(HumanPlayer.class));
   }
 
   @Test
   public void humanVsComputerChoice() {
-    List<Player> players = factory.getPlayersForChoice("2");
+    List<Player> players = factory.getPlayersForChoice(PlayerFactory.HVC);
     assertThat(players.get(0), instanceOf(HumanPlayer.class));
     assertThat(players.get(1), instanceOf(ComputerPlayer.class));
   }
 
   @Test
   public void computerVsHumanChoice() {
-    List<Player> players = factory.getPlayersForChoice("3");
+    List<Player> players = factory.getPlayersForChoice(PlayerFactory.CVH);
     assertThat(players.get(0), instanceOf(ComputerPlayer.class));
     assertThat(players.get(1), instanceOf(HumanPlayer.class));
   }
 
   @Test
   public void computerVsComputerChoice() {
-    List<Player> players = factory.getPlayersForChoice("4");
+    List<Player> players = factory.getPlayersForChoice(PlayerFactory.CVC);
     assertThat(players.get(0), instanceOf(ComputerPlayer.class));
     assertThat(players.get(1), instanceOf(ComputerPlayer.class));
+  }
+
+  @Test
+  public void returnsAllGameChoicesDescriptions() {
+    assertThat(PlayerFactory.allGameChoices().get(PlayerFactory.HVH), is(PlayerFactory.HUMAN_VS_HUMAN));
+    assertThat(PlayerFactory.allGameChoices().get(PlayerFactory.HVC), is(PlayerFactory.HUMAN_VS_COMPUTER));
+    assertThat(PlayerFactory.allGameChoices().get(PlayerFactory.CVH), is(PlayerFactory.COMPUTER_VS_HUMAN));
+    assertThat(PlayerFactory.allGameChoices().get(PlayerFactory.CVC), is(PlayerFactory.COMPUTER_VS_COMPUTER));
   }
 }
